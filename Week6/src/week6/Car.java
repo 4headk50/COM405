b65f4d53a7c744d23cd020797f4237cf7f5c863c
Kevin Head <4headk50@solent.ac.uk>
2019-10-29 16:05:59 +0000

    Added TicketFare to Car class and added a CarPark class

--- Week6/src/week6/Car.java
+++ Week6/src/week6/Car.java
@@ -10,5 +10,24 @@ package week6;
  * @author 4headk50
  */
 public class Car {
+    private String numberPlate;
+    private String colour;
+
+    //constructor
+    public Car(String numberPlateIn, String colourIn)
+    {
+        numberPlate = numberPlateIn;
+        colour = colourIn;
+    }
+    
+    //getter
+    public String GetNumberPlate ()
+  {
+     return numberPlate;
+  }
+    public String GetColour ()
+    {
+      return colour;
+    }
     
 }
--- Week6/src/week6/CarPark.java
+++ Week6/src/week6/CarPark.java
@@ -10,5 +10,24 @@ package week6;
  * @author 4headk50
  */
 public class CarPark {
+    private Car[] cars;
+    
+    public CarPark()
+    {
+        cars = new Car[15];
+    }
+    
+      public boolean carsParked (Car CarsIn)
+    {
+        for(int i=0; i< cars.length; i+=1)
+        {
+            if(cars[i] == null)
+            {
+                cars[i] = CarsIn;
+                return true;
+            }
+        }
+        return false;
+    }
     
 }
